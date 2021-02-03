052d0bed58a53cc0b08d442fe05ed93b68d0866b
Hurly77 <cameron.leverett@yahoo.com>
2021-02-03 02:14:28 -0800

    shop router to / now path and __dirname

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const path = require('path')
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.sendFile('./veiws/');
+	res.sendFile(path.join(__dirname, 'views'));
 });
 
 module.exports = router;
