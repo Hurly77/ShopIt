1b96c77e614fea1659191dfd141a4a4853e7d37c
Hurly77 <cameron.leverett@yahoo.com>
2021-02-04 00:40:06 -0800

    logging imported data from admin

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path')
 const root = require('../util/path')
 
+const adminData = require('./admin')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+	console.log(adminData.products)
 	res.sendFile(path.join(root, 'views', 'shop.html'));
 });
 
