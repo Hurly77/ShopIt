37f9f78042f8c64398bbe9ad7a08a7b205a07145
Hurly77 <cameron.leverett@yahoo.com>
2021-02-04 00:39:16 -0800

    added adminData import

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const bodyParser = require('body-parser')
 const app = express();
 
 const shopRoutes = require('./routes/shop')
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(shopRoutes)
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes)
 
 app.use((req, res, next)=>{
 	res.status(404).sendFile(path.join(root, 'views', '404.html'))
