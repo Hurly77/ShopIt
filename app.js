65b6c252a6713285e7cf6d89858bfad93f2f519c
cameron <cameron.leverett@yahoo.com>
2021-02-06 14:04:05 -0800

    added error controler, and changed naming

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const path = require('path')
-const root = require('./util/path')
+const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-const shopRoutes = require('./routes/shop')
-const adminData = require('./routes/admin')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(shopRoutes)
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-	res.status(404).sendFile(path.join(root, 'views', '404.html'))
- })
+app.use(errorController.get404);
 
 app.listen(3000);
