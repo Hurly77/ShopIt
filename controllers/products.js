b0187dd3da8a00e56a447d3f326a70ace1de9eb8
cameron <cameron.leverett@yahoo.com>
2021-02-06 21:58:40 -0800

    refactored

--- controllers/products.js
+++ controllers/products.js
@@ -16,15 +16,16 @@ exports.postAddProduct = (req, res, next) => {
 	res.redirect('/');
 };
 
-exports.getProducts =  (req, res, next) => {
-	const products = Product.fetchAll();
-  console.log('product controller', products)
-	res.render('shop', {
-		prods       : products,
-		pageTitle   : 'Shop',
-		path        : '/',
-		hasProducts : products.length > 0,
-		activeShop  : true,
-		productCSS  : true,
+exports.getProducts = (req,  res, next) => {
+	Product.fetchAll((products) => {
+		console.log('product controller', products);
+		res.render('shop', {
+			prods       : products,
+			pageTitle   : 'Shop',
+			path        : '/',
+			hasProducts : products.length > 0,
+			activeShop  : true,
+			productCSS  : true,
+		});
 	});
 };
