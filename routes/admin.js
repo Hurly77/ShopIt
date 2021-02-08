799604557a6e40475dc86d4ecc95483a3db96e2a
cameron <cameron.leverett@yahoo.com>
2021-02-07 16:04:48 -0800

    add new routes to admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,11 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
 
+// /admin/add-product => GET
+router.get('/products');
+
 // /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
+
 
 module.exports = router;
