259bb7e9eff75d713279a8fe842906dfe9936318
cameron <cameron.leverett@yahoo.com>
2021-02-07 19:48:25 -0800

    updated app

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,3 @@
-const { render } = require('pug');
 const Product = require('../models/product');
 
 exports.getProducts = (req,  res, next) => {
@@ -10,6 +9,17 @@ exports.getProducts = (req,  res, next) => {
 		});
 	});
 };
+
+exports.getProduct = (req, res, next) => {
+	const prodId = req.params.productId
+	Product.findById(prodId, product => {
+		res.render('shop/product-detail', {
+			product: product,
+			pageTitle: product.title,
+			path: '/products'
+		})
+	});
+}
 
 exports.getIndex = (req, res, next) => {
 	Product.fetchAll((products) => {
@@ -26,6 +36,11 @@ exports.getCart = (req, res, next) => {
 		path: '/cart',
 		pageTitle: 'Your Cart',
 	})
+}
+
+exports.postCart = (req, res, next) => {
+	const prodId = req.body.productId
+	res.redirect('/cart')
 }
 
 exports.getOrders = (req, res, next) => {
