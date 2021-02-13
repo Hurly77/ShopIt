a449473d23c89146b7783e93e6e9cd6ada30e8d2
cameron <cameron.leverett@yahoo.com>
2021-02-13 14:04:11 -0800

    inching toward this project  with a cart class

--- models/cart.js
+++ models/cart.js
@@ -1,7 +1,21 @@
+const fs = require('fs')
+
+const path = require('path')
+
 module.exports = class Cart {
   static addProduct(id) {a
     //fetch previous cart
+    fs.readFile(p,(err, fileContent) => {
+      let cart = {products: [], totalPrice: 0};
+      if (!err) {
+        cart = JSON.parse(fileContent)
+      }
+    })
     //anal the => Find existing product
+    const existingProduct = cart.products.find(prod => prod.id === id)
+    if(existingProduct) {
+      
+    }
     // add new 
     
   }
