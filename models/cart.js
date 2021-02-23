668c53e59b7ffb5dae9943f59c431c23360e3c3c
C.Jay Levey <59129546+Hurly77@users.noreply.github.com>
2021-02-22 23:48:44 -0800

    misc

--- models/cart.js
+++ models/cart.js
@@ -36,6 +36,7 @@ module.exports = class Cart {
 			cart.totalPrice = cart.totalPrice + productPrice;
 			fs.writeFile(p, JSON.stringify(cart), (err) => {
 				console.log(err);
+				
 			});
 		});
 	}
